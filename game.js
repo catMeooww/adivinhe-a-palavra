4e9a0ba598a5fd9f22145939e5ec501801093505
catMeooww <110690475+catMeooww@users.noreply.github.com>
2022-12-15 15:57:07 -0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,5 +1,48 @@
-p1Name = localStorage.getItem("p1");
-p2Name = localStorage.getItem("p2");
+function loadPlayers() {
+    player1Name = localStorage.getItem("player1");
+    player2Name = localStorage.getItem("player2");
 
-document.getElementById("p1").innerHTML = p1Name;
-document.getElementById("p2").innerHTML = p2Name;
+    console.log("players: " + player1Name + " | " + player2Name);
+
+    player1Score = 0;
+    player2Score = 0;
+
+    document.getElementById("name1").innerText = player1Name + " : ";
+    document.getElementById("name2").innerText = player2Name + " : ";
+
+    document.getElementById("score1").innerText = player1Score;
+    document.getElementById("score2").innerText = player2Score;
+
+    document.getElementById("playerQ").innerText = "Turno de Pergunta - " + player1Name;
+    document.getElementById("playerA").innerText = "Turno de Resposta - " + player2Name;
+}
+function send() {
+    getWord = document.getElementById("word").value;
+    word = getWord.toLowerCase();
+    console.log(getWord + "|" + word);
+
+    charAt1 = word.charAt(Math.random(word.lenght));
+    console.log(charAt1);
+
+    lenghtDivide2 = Math.floor(word.length / 2);
+    charAt2 = word.charAt(lenghtDivide2);
+    console.log(lenghtDivide2 + "|" + charAt2);
+
+    lenghtMinus1 = word.length - 1;
+    charAt3 = word.charAt(lenghtMinus1);
+    console.log(charAt3);
+
+    removeCharAt1 = word.replace(charAt1, "_");
+    removeCharAt2 = word.replace(charAt2, "_");
+    removeCharAt3 = word.replace(charAt3, "_");
+    console.log(removeCharAt3);
+
+    question_word = "<h4 id='wordDisplay'> P. " + word.replace(charAt1, "_").replace(charAt2, "_").replace(charAt3, "_") + "</h4>";
+    inputBox = "<br>Resposta : <input type='text' id='inputCheckBox'>";
+    checkButton = "<br><br><button class='btn btn-info' onclick='check()'>Checar</button>";
+    row = question_word + inputBox + checkButton;
+
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("word").value = "";
+
+}
--- game_login.js
+++ game_login.js
@@ -7,8 +7,8 @@ p2 = document.getElementById("p2").value;
 console.log("página 1:  "+p1);
 console.log("página 1:  "+p2);
 
-localStorage.setItem("p1",p1);
-localStorage.setItem("p2",p2);
+localStorage.setItem("player1",p1);
+localStorage.setItem("player2",p2);
 
 window.location = "game.html";
 }
